e9c8f7ba5009f24c738e55383f2c2d649fa7c188
Pratyush Dev <pratyushdev20@gmail.com>
2026-07-27 11:56:25 +0530

    connect to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+// import expreess from "express";
+
+// const app = express();
+// ( async () => {
+//     try {
+//         mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(erroe) => {
+//             console.log("ERRR: ",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () =>{
+//             console.log(`app is listening on port ${process.env.PORT}`)
+//         })
+
+//     }catch (error){
+//         console.error("ERROR: ",error)
+//         throw err
+//     }
+// })
+
+//this is way to connect database in index.js without using another folder
+
+
+
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path :'./env'
+})
+connectDB();
